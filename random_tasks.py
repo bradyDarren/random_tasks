a17abc8eef5c93b25e43408cbf192e05b9ff3c81
bradyDarren <dcgbfinancial@gmail.com>
2024-10-10 04:57:06 -0500

    adding while loop

--- random_tasks.py
+++ random_tasks.py
@@ -1,26 +1,21 @@
 import random
 
-assignment = ["Project Management", "Data Analysis", "Customer Support", "Marketing Campaign", 
-    "Software Development", "Product Design", "Technical Writing", "Sales Outreach", 
-    "Quality Assurance", "Human Resources Training", "Financial Auditing", "Client Presentation"]
+assignment = []
 
-employees = ["John Smith", "Jane Doe", "Michael Johnson", "Emily Davis", "Robert Wilson", 
-    "Linda Martinez", "James Brown", "Patricia Taylor", "David Anderson", 
-    "Barbara Moore", "Richard Thomas", "Jessica Harris", "Charles White", 
-    "Susan Clark", "Joseph Lewis", "Sarah Lee", "Daniel Walker", "Karen Hall"]
+employees = []
 
 # prompting the user to input the assignments needed to be distributed
-# user_assignment = input("Input the tasks needed to be assigned. Please seperate by a comma. : ")
+user_assignment = input("Input the tasks needed to be assigned. Please seperate by a comma. : ")
 
-# for task in user_assignment.split(","):
-#     assignment.append(task.strip())
-# print(assignment)
+for task in user_assignment.split(","):
+    assignment.append(task.strip())
+print(assignment)
 
-# # prompting the user to input the employes that assignments can be distributed to.
-# # user_employees = input("Input the name of the employees needed to be assigned. Please seperate by a comma. : ")
+# prompting the user to input the employes that assignments can be distributed to.
+user_employees = input("Input the name of the employees needed to be assigned. Please seperate by a comma. : ")
 
-# for emp in user_employees.split(","):
-#     employees.append(emp.strip())
+for emp in user_employees.split(","):
+    employees.append(emp.strip())
 
 while employees or assignment:
     if employees == []:
